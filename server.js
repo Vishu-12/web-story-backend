c6e202cc33e6f92a6c300322148f144ae6c6829d
Vishu-12 <garg.vishu810@gmail.com>
2024-09-27 12:14:58 +0530

    allow all access controls

--- server.js
+++ server.js
@@ -22,7 +22,14 @@ app.use(
     origin: "*",
   })
 );
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use((req, res, next) => {
   const log = `${req.method} - ${req.url} - ${req.ip} - ${new Date()}/n`;
   fs.appendFile("log.txt", log, (err) => {
